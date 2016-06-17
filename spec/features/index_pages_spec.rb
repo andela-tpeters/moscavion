b09c437455126529873857d6a51dca586d2340e5
Tijesunimi Peters <tijesunimi.peters@andela.com>
2016-06-17 21:17:24 +0100

    feat(index-form): added flight search form [start #121742041]

--- spec/features/index_pages_spec.rb
+++ spec/features/index_pages_spec.rb
@@ -13,9 +13,27 @@ RSpec.feature "IndexPages", type: :feature do
 		end
 
 		it 'should have links' do
-			expect(page.has_link? "Contact Us").to be_truthy
-			expect(page.has_link? "About Us").to be_truthy
-			expect(page.has_link? "Help").to be_truthy
+			within ".ui.secondary.pointing.menu" do
+				expect(page.has_link? "Contact Us").to be_truthy
+				expect(page.has_link? "About Us").to be_truthy
+				expect(page.has_link? "Help").to be_truthy
+			end
+		end
+
+		it "should have flight search form" do
+			expect(page).to have_selector "form#flights_search_form"
+			
+			within('#flights_search_form_cont') do
+				search_form = page.find("#flights_search_form")
+				expect(search_form.tag_name == "form").to be_truthy
+				within search_form do
+					expect(page.has_field? "from").to be_truthy
+					expect(page.has_field? "to").to be_truthy
+					expect(page.has_field? "passengers").to be_truthy
+					expect(page.has_field? "departure_date").to be_truthy
+					expect(page.has_button?("Search Flight")).to be_truthy
+				end
+			end
 		end
 	end
 end
