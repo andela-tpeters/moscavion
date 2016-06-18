899d6b59af8a2d26179fd0e8f30af1614737bcf5
Tijesunimi Peters <tijesunimi.peters@andela.com>
2016-06-18 03:06:04 +0100

    chore: resolving conflict

---
+++ spec/models/flight_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Flight, type: :model do
+	describe "a specification" do
+		it 'does something' do
+			flight = create(:flight)
+			binding.pry
+		end
+	end
+end
