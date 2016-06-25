cfddda322a736b2af6d7db0d4a0b030a22cb5dc4
Tijesunimi Peters <tijesunimi.peters@andela.com>
2016-06-25 11:24:38 +0100

    feat(chore): updated airline spec [start #122263031]

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -34,14 +34,35 @@ RSpec.describe Airline, type: :model do
 	  end
 	end
 
-	describe 'airport association' do
+	describe 'association' do
 	  it 'belongs to airport' do
 	  	airline = create(:airline)
 	  	expect(airline.airport.name).to eql("Heathrow")
 	  end
 
+	  it 'updates airline' do
+	  	airline = create :airline
+	  	airline_from_db = Airline.find_by(:id => 1)
+	  	expect(airline_from_db.name).to eq("Arik")
+
+	  	airline_from_db.name = "Sosoliso Airline"
+	  	airline_from_db.icao = "SSS"
+	  	airline_from_db.iata = "SSA"
+	  	airline_from_db.save
+
+	  	airline = Airline.find_by(:id => 1)
+	  	expect(airline.name).to eq("Sosoliso Airline")
+	  end
+
 	  it 'updates airport' do
-	  	
+	  	airline = create :airline
+	  	expect(airline.airport.name).to eql("Heathrow")
+
+	  	airline = Airline.first
+	  	airport = create(:airport, :name => "Ibadan", :id => 2)
+	  	airline.airport = airport
+	  	airline.save
+	  	expect(Airline.first.airport.name).to eq("Ibadan")
 	  end
 	end
 end
