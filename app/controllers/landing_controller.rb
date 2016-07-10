cc0a99a0ee6c86c2a1c0b2eb5d682fce927db6e7
Tijesunimi Peters <tijesunimi.peters@andela.com>
2016-07-10 09:21:45 +0100

    Feat: index UI/UX complete with search result [finish #125236231][finish #125989107]

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,6 @@
 class LandingController < ApplicationController
 	def index
+		@departures = Flight.select(:departure_location).distinct(:departure_location)
+		@arrivals = Flight.select(:arrival_location).distinct(:arrival_location)
 	end
 end
